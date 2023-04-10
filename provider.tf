1cd92b71cf592841c9d31aef9e27b81fa1565c5c
TheFinalHawk <39245639+TheFinalHawk@users.noreply.github.com>
2023-04-10 17:17:04 -0400

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -8,9 +8,5 @@ terraform {
 }
 
 provider "azurerm" {
-  subscription_id = "d9ff5d97-c0e9-49b2-835d-fc0f8396d5a8"
-  client_id       = "3548fd25-85e2-4825-bb62-ae3eebb8dc8a"
-  client_secret   = "fOg8Q~GM5YTrx49U9tlZe4KK_v2IkUqkl3qbTbPv"
-  tenant_id       = "456a7a24-a336-42a9-9116-36b62d285483"
-  features {}
+#removed for security
 }
